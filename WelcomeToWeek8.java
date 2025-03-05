3e0113cec4d3e69a85ece69646050e78bff540b0
Anchal2003 <sahuanchal22@gmail.com>
2025-03-05 14:54:44 +0530

    [Anchal Sahu] Add UC3- Refactor the Code to write a function to get work hours.

--- WelcomeToWeek8.java
+++
@@ -1,6 +0,0 @@
-//Create a WelcomeToWeek8 to print the welcome statement
-public class WelcomeToWeek08 {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to Week-08!\"");
-    }
-}
